58f2a2757a60361d8215dfc731cd0ba5912d1456
Muskan <sharmamuskan17017@gmail.com>
2025-03-15 11:17:22 +0530

    modify index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { router as userRouter } from './routers/userRoutes.js';
 import { router as jobRouter } from './routers/jobRoutes.js';
-import { router as bidRouter } from './routers/bidRoutes.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './db.js';
@@ -14,7 +13,6 @@ app.use(express.json());
 
 app.use('/api/users', userRouter);
 app.use('/api/jobs', jobRouter);
-app.use('/api/bids', bidRouter);
 
 const port = process.env.PORT || 5000;
 
